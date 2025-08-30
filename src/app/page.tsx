b67fbc857e18c61afba11c8019a4a8e6f648b5b4
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-08-30 23:33:23 +0200

    Add POST

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,11 @@ export default function ExpensePage() {
   const [expenses, setExpenses] = useState<Expense[]>([]);
   const [loading, setLoading] = useState(true);
 
+  const [title, setTitle] = useState("");
+  const [amount, setAmount] = useState("");
+  const [category, setCategory] = useState("");
+  const [date, setDate] = useState("");
+
   const fetchExpense = async () => {
     try {
       const res = await fetch("/api/expenses");
@@ -26,17 +31,84 @@ export default function ExpensePage() {
     fetchExpense();
   }, []);
 
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const newExpense = { title, amount: parseFloat(amount), category, date };
+
+    try {
+      const res = await fetch("/api/expenses", {
+        method: "POST",
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify(newExpense),
+      });
+
+      if (!res.ok) throw new Error("Failed to create expense");
+
+      const saved: Expense = await res.json();
+
+      setExpenses((prev) => [...prev, saved]);
+
+      setTitle("");
+      setAmount("");
+      setCategory("");
+      setDate("");
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   if (loading) return <p>Loading...</p>;
 
   return (
     <div className="max-w-2xl mx-auto p-4">
       <h1 className="text-xl font-bold mb-4">Expenses</h1>
+
+      <form onSubmit={handleSubmit} className="mb-6 space-y-2" >
+        <input
+          type="text"
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          className="p-2 border rounded w-full"
+        />
+        <input
+          type="number"
+          step={0.01}
+          placeholder="Amount"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+          className="p-2 border rounded w-full"
+        />
+        <input
+          type="text"
+          placeholder="Category"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+          className="p-2 border rounded w-full"
+        />
+        <input
+          type="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+          className="p-2 border rounded w-full"
+        />
+        <button type="submit" className="bg-blue-500 px-2 py-1 rounded">
+          Add Expense
+        </button>
+
+      </form>
+
       {expenses.length === 0 ? (
         <p>No expenses yet</p>
       ) : (
         <ul className="space-y-2">
           {expenses.map((exp) => (
-            <li key={exp.id} className="flex justify-between items-center border p-2 rounded">
+            <li
+              key={exp.id}
+              className="flex justify-between items-center border p-2 rounded"
+            >
               <span>
                 {exp.title} - {exp.category}
               </span>
