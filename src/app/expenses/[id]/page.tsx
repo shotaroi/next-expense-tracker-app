5601c24f630c12c148994792675b4bfc7f89d7be
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-10-06 01:54:01 +0200

    Update page.tsx

--- src/app/expenses/[id]/page.tsx
+++ src/app/expenses/[id]/page.tsx
@@ -1,8 +1,10 @@
 // src/app/expenses/[id]/page.tsx
 export default async function ExpensePage({ params }: { params: { id: string } }) {
+    const {id} = await params;
+
   return (
     <main className="p-6 max-w-2xl mx-auto">
-      <h1 className="text-2xl font-bold">Expense {params.id}</h1>
+      <h1 className="text-2xl font-bold">Expense {id}</h1>
     </main>
   );
 }
